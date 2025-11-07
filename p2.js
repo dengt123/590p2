6174a7091f613145552cd0af4c88237efa6dcb1d
Timothy Deng <dengt@ad.unc.edu>
2025-11-07 17:00:48 -0500

    Add files via upload

---
+++ p2.js
@@ -0,0 +1,87 @@
+console.clear();
+
+// ----------------------------------------------
+// Axis data (do not modify)
+// ----------------------------------------------
+
+let A = [
+    [0.0, 0.0, 0.0],
+    [1.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0],
+    [0.0, 0.0, 0.0],
+    [0.0, 0.0, 1.0]
+];
+
+// ----------------------------------------------
+// end axis data
+// ----------------------------------------------
+
+// ----------------------------------------------
+// Simuation control (do not modify)
+// ----------------------------------------------
+
+let xang = 0;
+let yang = 0;
+let zang = 0;
+let rot = 0;
+let axisRotation = null;
+let rot_inc = 10;
+
+function startRotation(rotationFunc) {
+    if (axisRotation !== null) clearInterval(axisRotation);
+    axisRotation = setInterval(rotationFunc, 100);
+}
+
+function stopRotation() {
+    clearInterval(axisRotation);
+    axisRotation = null;
+}
+
+document.addEventListener('mouseup', stopRotation);
+
+document.addEventListener('mousedown', function (event) {
+    switch ( event.target.id ) {
+        case "pitch-up":
+            startRotation(() => { xang = ( xang + rot_inc ) % 360; });
+            break;
+        case "pitch-down":
+            startRotation(() => { xang = ( xang - rot_inc ) % 360; });
+            break;
+        case "roll-left":
+            startRotation(() => { zang = ( zang + rot_inc ) % 360; });
+            break;
+        case "roll-right":
+            startRotation(() => { zang = ( zang - rot_inc ) % 360; });
+            break;
+        case "yaw-left":
+            startRotation(() => { yang = ( yang + rot_inc ) % 360; });
+            break;
+        case "yaw-right":
+            startRotation(() => { yang = ( yang - rot_inc ) % 360; });
+            break;
+        case "reset":
+            xang = yang = zang = 0; 
+            break;
+        default:
+            stopRotation();
+    }
+});
+
+// ----------------------------------------------
+// End simuation control
+// ----------------------------------------------
+
+function configure() {
+    canvas = document.getElementById("webgl-canvas");
+    webgl_context = canvas.getContext("webgl");
+    program = initShaders(webgl_context, "vertex-shader", "fragment-shader");
+    webgl_context.useProgram(program);
+
+    webgl_context.viewport(0,0,canvas.width, canvas.height);
+
+    attr_vertex = webgl_context.getAttribLocation(program, "vertex");
+    uniform_color = webgl_context.getUniformLocation(program, "color");
+    uniform_view = webgl_context.getUniformLocation(program, "View");
+}
+
